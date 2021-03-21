c1259ae6c35e904f882b67310316285fae455db4
Edygar de Lima Oliveira <edygardelima@gmail.com>
2021-03-21 23:58:39 +0000

    improvements on LSP + Code completion

--- nvim/lua/custom/lsp.lua
+++ nvim/lua/custom/lsp.lua
@@ -1,13 +1,82 @@
-require'lspinstall'.setup()
+local lspconfig = require'lspconfig'
 
-local custom_attach = function(client)
-  print("'" .. client.name .. "' language server started" );
-
-  require'completion'.on_attach(client)
-  require'diagnostic'.on_attach(client)
+local function setup_servers()
+  require'lspinstall'.setup()
+  local servers = require'lspinstall'.installed_servers()
+  for _, server in pairs(servers) do
+    lspconfig[server].setup{}
+  end
 end
 
-local servers = require'lspinstall'.installed_servers()
-for _, server in pairs(servers) do
-  require'lspconfig'[server].setup{ on_attach=custom_attach }
+setup_servers()
+
+-- Automatically reload after `:LspInstall <server>` so we don't have to restart neovim
+require'lspinstall'.post_install_hook = function ()
+  setup_servers() -- reload installed servers
+  vim.cmd('bufdo e') -- this triggers the FileType autocmd that starts the server
 end
+
+local eslint = {
+  lintCommand = 'eslint_d -f unix --stdin --stdin-filename ${INPUT}',
+  lintStdin = true,
+  lintFormats = {'%f:%l:%c: %m'},
+  lintIgnoreExitCode = true,
+  formatCommand = 'eslint_d --fix-to-stdout --stdin --stdin-filename=${INPUT}',
+  formatStdin = true
+}
+
+local function eslint_config_exists()
+  local eslintrc = vim.fn.glob(".eslintrc*", 0, 1)
+
+  if not vim.tbl_isempty(eslintrc) then
+    return true
+  end
+
+  if vim.fn.filereadable("package.json") then
+    if vim.fn.json_decode(vim.fn.readfile("package.json"))["eslintConfig"] then
+      return true
+    end
+  end
+
+  return false
+end
+
+lspconfig.efm.setup {
+  on_attach = function(client)
+    client.resolved_capabilities.document_formatting = true
+    client.resolved_capabilities.goto_definition = false
+  end,
+  root_dir = function()
+    if not eslint_config_exists() then
+      return nil
+    end
+    return vim.fn.getcwd()
+  end,
+  settings = {
+    languages = {
+      javascript = {eslint},
+      javascriptreact = {eslint},
+      ["javascript.jsx"] = {eslint},
+      typescript = {eslint},
+      ["typescript.tsx"] = {eslint},
+      typescriptreact = {eslint}
+    }
+  },
+  filetypes = {
+    "javascript",
+    "javascriptreact",
+    "javascript.jsx",
+    "typescript",
+    "typescript.tsx",
+    "typescriptreact"
+  },
+}
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+vim.lsp.diagnostic.on_publish_diagnostics, {
+  signs = true,
+  underline = true,
+  update_in_insert = true,
+  virtual_text = false,
+}
+)
