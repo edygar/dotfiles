df34b9faab23c08da7c07f0b00e0a3adcfc34055
Edygar de Lima Oliveira <edygardelima@gmail.com>
2022-07-19 14:12:02 +0100

    Tmux jump + deps

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 if [ ! -d "$HOME/.dotfiles" ]; then
-    echo "=== Install devtools if needed ==="
-    xcode-select --install
+	echo "=== Install devtools if needed ==="
+	xcode-select --install
 
-    echo "=== Installing .dotfiles for the first time ==="
-    git clone --depth=1 https://github.com/edygar/dotfiles.git "$HOME/.dotfiles"
-    cd "$HOME/.dotfiles"
+	echo "=== Installing .dotfiles for the first time ==="
+	git clone --depth=1 https://github.com/edygar/dotfiles.git "$HOME/.dotfiles"
+	cd "$HOME/.dotfiles"
 else
-    cd "$HOME/.dotfiles"
+	cd "$HOME/.dotfiles"
 fi
 
 echo "=== Installing Brew ==="
@@ -21,7 +21,7 @@ brew bundle
 echo "=== Installing ZSH ==="
 export ZSH="$HOME/.dotfiles/oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-curl -L https://raw.githubusercontent.com/sbugzu/gruvbox-zsh/master/gruvbox.zsh-theme > "$ZSH/custom/themes/gruvbox.zsh-theme"
+curl -L https://raw.githubusercontent.com/sbugzu/gruvbox-zsh/master/gruvbox.zsh-theme >"$ZSH/custom/themes/gruvbox.zsh-theme"
 
 echo "=== iTerm Setup ==="
 defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$HOME/.dotfiles/iTerm2"
@@ -34,18 +34,18 @@ ln -nfs $HOME/.dotfiles/nvim $HOME/.config/nvim
 
 echo "=== Link config files ==="
 for file in vimrc zshrc gitconfig ctags tmux.conf; do
-  if [[ -f "$HOME/.$file" ]]; then
-    mv "$HOME/.$file" "$HOME/.$file.bkp"
-  fi
+	if [[ -f "$HOME/.$file" ]]; then
+		mv "$HOME/.$file" "$HOME/.$file.bkp"
+	fi
 
-  ln -nfs $HOME/.dotfiles/$file $HOME/.$file
-done;
+	ln -nfs $HOME/.dotfiles/$file $HOME/.$file
+done
 
 source $HOME/.dotfiles/nvm.zsh
 
 echo "=== Installing Node ==="
 if [[ ! -f "$HOME/.nvm" ]]; then
-    mkdir $HOME/.nvm
+	mkdir $HOME/.nvm
 fi
 
 source $HOME/.dotfiles/nvm.zsh
@@ -63,3 +63,5 @@ npm install -g neovim prettier typescript typescript-language-server eslint esli
 
 echo "=== Fetching Oh-My-Zsh custom plugins ==="
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/schasse/tmux-jump ~/.tmux/plugins/tpm
