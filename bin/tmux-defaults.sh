22b01f90297c1e807fc9eeef7668ca7f2583e74d
Edygar de Lima Oliveira <edygardelima@gmail.com>
2022-08-11 17:26:45 +0100

    tmux-sessionizer now sorts by last usage

--- bin/tmux-defaults.sh
+++ bin/tmux-defaults.sh
@@ -8,3 +8,4 @@ export FZF_LAYOUT="
 --prompt=' '
 --pointer=' '
 "
+export FZF_DEFAULT_OPTS="$FZF_LAYOUT"
--- bin/tmux-sessionizer.sh
+++ bin/tmux-sessionizer.sh
@@ -1,21 +1,34 @@
 #!/usr/bin/env bash
 DEV_HOME=$HOME/dev
+HISTORY_FILE=$HOME/.tmux-sessions-history
 source "$(cd "$(dirname "$0")" && pwd)/tmux-defaults.sh"
 
 cd $DEV_HOME
 
-session=$(find * -mindepth 1 -maxdepth 1 \( -type l -o -type d \) | cat - <(echo "Home") | FZF_DEFAULT_OPTS="$FZF_LAYOUT" fzf --preview='echo "{}" | tr "/" " " | tr "." "_" | xargs tmux capture-pane -ep -t 2>/dev/null || echo "Session not started yet"')
+if [ ! -e HISTORY_FILE ]; then
+	touch "$HISTORY_FILE"
+fi
+
+current_session_name=$(tmux display-message -p '#S')
+current_session=$(echo $current_session_name | tr " " "/" | tr "_" ".")
+
+sessions=$(find * -mindepth 1 -maxdepth 1 \( -type l -o -type d \) | cat - <(echo "Home"))
+sessions=$(cat "$HISTORY_FILE" | grep -v -x -F <(echo "$sessions") | cat - <(echo -e "$sessions") | awk '!a[$0]++' | grep -v -x -F $(echo "$current_session"))
+session=$(echo "$sessions" | FZF_DEFAULT_OPTS="$FZF_LAYOUT" fzf --tiebreak=index --preview='echo "{}" | tr "/" " " | tr "." "_" | xargs tmux capture-pane -ep -t 2>/dev/null || echo "Session not started yet"')
 
 session_name=$(echo $session | tr "/" " " | tr "." "_")
 tmux_running=false
 
-if [ "$session" == "Home" ]; then
-  session_name="Home"
-  session="$HOME"
-fi
-
 if [ -z "$session" ]; then
 	exit 0
+fi
+
+if [ "$session" == "Home" ]; then
+	session_name="Home"
+	session="$HOME"
+  echo -e "Home\n$current_session\n$sessions" | awk '!a[$0]++' >"$HISTORY_FILE"
+else
+  echo -e "$session\n$current_session\n$sessions" | awk '!a[$0]++' >"$HISTORY_FILE"
 fi
 
 if tmux info &>/dev/null; then
