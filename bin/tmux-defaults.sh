44013562119c03d3aca3e15dc0545881ed7ce263
Edygar de Lima Oliveira <edygar@users.noreply.github.com>
2022-08-11 18:05:21 +0100

    Adds transparency to all backgrounds

--- bin/tmux-defaults.sh
+++ bin/tmux-defaults.sh
@@ -3,7 +3,7 @@ export FZF_LAYOUT="
 --layout=reverse
 --no-info
 --keep-right
---color='fg:#abb2bf,bg:#1b1f27,gutter:#1C2026,preview-bg:#1C2026,pointer:#44A5F9,bg+:#042E48,fg+:#abb2bf,hl:#d5b06b,hl+:#d5b06b'
+--color='fg:#abb2bf,gutter:#1C2026,pointer:#44A5F9,fg+:#abb2bf,hl:#d5b06b,hl+:#d5b06b'
 --preview-window='right,75%,border-rounded'
 --prompt=' '
 --pointer=' '
