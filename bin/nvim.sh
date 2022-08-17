01aaab81cd29c4098eab84f2b943f0bf898df231
Edygar de Lima Oliveira <edygar@users.noreply.github.com>
2022-08-17 06:24:39 +0100

    Fix recenlty used sessions switch

--- bin/nvim.sh
+++ bin/nvim.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-LISTEN="/tmp/nvim.$(tmux display-message -p '#{session_id}_#I_#P').pipe"
-tmux rename-window nvim
+LISTEN="/tmp/nvim.$(/usr/local/bin/tmux display-message -p '#{session_id}_#I_#P').pipe"
+/usr/local/bin/tmux rename-window nvim
+/usr/local/bin/tmux select-pane -T "NeoVim"
 /usr/local/bin/nvim --listen "$LISTEN" "$@"
--- bin/tmux-sessionizer.sh
+++ bin/tmux-sessionizer.sh
@@ -15,23 +15,28 @@ sessions=$(echo -e "$(fd -a --min-depth 2 --max-depth 2 --type l --type d -x "$B
 
 sessions_names=$(echo "$sessions" | awk -F "\t" '{print $1}')
 sessions_names=$(grep -v -x -F <(echo "$sessions_names") <"$HISTORY_FILE" | cat - <(echo "$sessions_names") | awk '!a[$0]++' | grep -v -x -F "$current_session")
+tmux set pane-border-status top
+tmux select-pane -T "Select a session"
+pretty=$(echo -e "$sessions_names" | sed 's/^/\\033[37m/g;s/ /\\033[0m /g')
 session_name=$(
-	echo -e "$sessions_names" |
-		FZF_DEFAULT_OPTS="${FZF_LAYOUT//--keep-right/}" fzf --tiebreak=index \
+	echo -e "$pretty" |
+		FZF_DEFAULT_OPTS="${FZF_LAYOUT//--keep-right/}" fzf --tiebreak=index --ansi \
 			--preview='tmux capture-pane -ep -t '{}' 2>/dev/null || echo "Session not started yet"'
 )
 
+tmux set pane-border-status off
 if [ -z "$session_name" ]; then
 	exit 0
 fi
 
 session_name=$(echo "$session_name" | xargs)
-
 session_path="$(echo -e "$sessions" | grep -F "${session_name//\\s/\\ /}" | awk -F "\t" '{print $2}')"
 
 if tmux info &>/dev/null; then
 	tmux_running=true
 fi
+
+echo -e "$session_name\n$current_session" | cat - <(echo "$sessions_names" | grep -v -x -F "$session_name") >"$HISTORY_FILE"
 
 if ! tmux has-session -t "$session_name" 2>/dev/null; then
 	tmux new-session -s "$session_name" -c "$session_path" "$([ $tmux_running = true ] && echo "-d")"
@@ -42,4 +47,3 @@ if [ $tmux_running == true ] && [ -n "$TMUX" ]; then
 else
 	tmux attach-session -t "$session_name"
 fi
-echo -e "$session_name" >>"$HISTORY_FILE"
