70fb64a5a272f6de0a8d8f84edc5172dd703c634
Edygar de Lima Oliveira <edygar@users.noreply.github.com>
2026-07-08 16:11:41 +0100

    Fix front_app icon color: white instead of bar background color

--- .config/sketchybar/items/front_app.sh
+++ .config/sketchybar/items/front_app.sh
@@ -2,7 +2,7 @@
 
 sketchybar --add item front_app left \
            --set front_app       background.color=$ITEM_BG_COLOR \
-                                 icon.color=$BAR_COLOR \
+                                 icon.color=$WHITE \
                                  icon.font="sketchybar-app-font:Regular:16.0" \
                                  label.color=$WHITE \
                                  script="$PLUGIN_DIR/front_app.sh"            \
