25bf5b638e3a70d4aa363903a440c0b4950ac9df
Edygar de Lima Oliveira <edygar@users.noreply.github.com>
2026-07-08 12:14:30 +0100

    Add Raycast script to switch wallpaper on demand

---
+++ .local/bin/raycast-next-wallpaper.zsh
@@ -0,0 +1,10 @@
+#!/bin/bin/zsh
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Next Wallpaper
+# @raycast.description Switch to the next Portugal city skyline wallpaper
+# @raycast.icon 🏙️
+# @raycast.mode silent
+
+~/.local/bin/portugal-wallpaper.zsh next
